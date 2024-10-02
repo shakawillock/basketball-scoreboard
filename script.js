4c18bba16a5b49179639b3785cc613bef48676ec
Shaka <shakawillock@gmail.com>
2024-10-01 23:09:55 -0400

    Remove onclick and addEventListener to buttons

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let homeScoreEl = document.getElementById('home-score');
 let guestScoreEl = document.getElementById('guest-score');
 
+let homeButtonOne = document.getElementById('home-button-one')
+let homeButtonTwo = document.getElementById('home-button-two');
+let homeButtonThree = document.getElementById('home-button-three');
+
+let guestButtonOne = document.getElementById('guest-button-one');
+let guestButtonTwo = document.getElementById('guest-button-two');
+let guestButtonThree = document.getElementById('guest-button-three');
+
 let homeScore = 0;
 let guestScore = 0;
 
 homeScoreEl.textContent = homeScore;
 guestScoreEl.textContent = guestScore;
+
+homeButtonOne.addEventListener('click', add1PointToHome)
+homeButtonTwo.addEventListener('click', add2PointsToHome)
+homeButtonThree.addEventListener('click', add3PointsToHome)
+
+guestButtonOne.addEventListener('click', add1PointToGuest);
+guestButtonTwo.addEventListener('click', add2PointsToGuest);
+guestButtonThree.addEventListener('click', add3PointsToGuest);
+
 
 function add1PointToHome() {
   homeScore += 1;
